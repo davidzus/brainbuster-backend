62de18d9df9fe59215898d79e6914dd1a6e65982
davidzus <davidlaskowski50@gmail.com>
2025-10-09 12:26:24 +0200

    Allowed Cors Policy for 'http://localhost:5173'

--- src/main/java/org/example/brainbuster/controller/AuthController.java
+++ src/main/java/org/example/brainbuster/controller/AuthController.java
@@ -11,13 +11,11 @@ import org.example.brainbuster.service.JwtService;
 import org.example.brainbuster.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
